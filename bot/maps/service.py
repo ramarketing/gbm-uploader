2f0ede2f979b862b9fff89d636e05563870dfd4d
Irving Kcam <me@ikcam.com>
2019-08-06 13:20:03 -0500

    Adjust service

--- bot/maps/service.py
+++ bot/maps/service.py
@@ -19,29 +19,31 @@ class MapService(BaseService):
 
     def get_list(self, **kwargs):
         folder = os.getcwd()
-        for root, dirs, files in os.walk(folder):
-            for name in files:
-                if not name.endswith('.csv'):
-                    continue
+        for name in os.listdir(folder):
+            if (
+                os.path.isfile(os.path.join(folder, name)) or
+                not name.endswith('.csv')
+            ):
+                continue
 
-                file = os.path.join(folder, name)
-                data = [i for i in csv.DictReader(open(file, 'r'))]
-                valid_file = True
+            file = os.path.join(folder, name)
+            data = [i for i in csv.DictReader(open(file, 'r'))]
+            valid_file = True
 
-                for i in data:
-                    if 'directions' in i and i['directions']:
-                        valid_file = False
+            for i in data:
+                if 'directions' in i and i['directions']:
+                    valid_file = False
 
-                if not valid_file:
-                    continue
+            if not valid_file:
+                continue
 
-                print('Running file: "{}".'.format(file))
+            print('Running file: "{}".'.format(file))
 
-                data = {
-                    'next': None,
-                    'previous': None,
-                    'count': len(data),
-                    'results': data
-                }
+            data = {
+                'next': None,
+                'previous': None,
+                'count': len(data),
+                'results': data
+            }
 
-                return self.entity_list(self, data)
+            return self.entity_list(self, data)
