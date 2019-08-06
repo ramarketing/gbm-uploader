ab7e9e9b5be21fde400330553200ccb0cb4951c8
Irving Kcam <me@ikcam.com>
2019-08-06 13:37:09 -0500

    Fix file analyzer

--- bot/maps/run.py
+++ bot/maps/run.py
@@ -1,26 +1,37 @@
 import csv
 import os
 
-from ..config import BASE_DIR
-
 from .selenium import MapsSelenium
 from .service import MapService
 
 
 def run(*args, **kwargs):
-    maps_service = MapService()
-    object_list = maps_service.get_list()
+    folder = os.getcwd()
+    for name in os.listdir(folder):
+        if (
+            not os.path.isfile(os.path.join(folder, name)) or
+            not name.endswith('.csv')
+        ):
+            continue
 
-    for obj in object_list:
-        MapsSelenium(entity=obj)
+        file_name = os.path.join(folder, name)
+        maps_service = MapService()
+        object_list = maps_service.get_list(file=file_name)
 
-    with open(os.path.join(BASE_DIR, 'file.csv'), 'w') as file:
-        obj = object_list[0]
-
-        fieldnames = [h for h in obj.raw_data.keys()]
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
+        if not object_list:
+            continue
 
         for obj in object_list:
-            writer.writerow(obj.raw_data)
-    file.close()
+            MapsSelenium(entity=obj)
+
+        with open(file_name, 'w') as file:
+            obj = object_list[0]
+
+            fieldnames = [h for h in obj.raw_data.keys()]
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for obj in object_list:
+                writer.writerow(obj.raw_data)
+
+        file.close()
--- bot/maps/service.py
+++ bot/maps/service.py
@@ -1,5 +1,4 @@
 import csv
-import os
 
 from ..base.service import BaseEntity, BaseEntityList, BaseService
 
@@ -17,33 +16,24 @@ class MapService(BaseService):
     entity = Map
     entity_list = MapList
 
-    def get_list(self, **kwargs):
-        folder = os.getcwd()
-        for name in os.listdir(folder):
-            if (
-                not os.path.isfile(os.path.join(folder, name)) or
-                not name.endswith('.csv')
-            ):
-                continue
+    def get_list(self, file, **kwargs):
+        data = [i for i in csv.DictReader(open(file, 'r'))]
+        valid_file = True
 
-            file = os.path.join(folder, name)
-            data = [i for i in csv.DictReader(open(file, 'r'))]
-            valid_file = True
+        for i in data:
+            if 'directions' in i and i['directions']:
+                valid_file = False
 
-            for i in data:
-                if 'directions' in i and i['directions']:
-                    valid_file = False
+        if not valid_file:
+            return
 
-            if not valid_file:
-                continue
+        print('Running file: "{}".'.format(file))
 
-            print('Running file: "{}".'.format(file))
+        data = {
+            'next': None,
+            'previous': None,
+            'count': len(data),
+            'results': data
+        }
 
-            data = {
-                'next': None,
-                'previous': None,
-                'count': len(data),
-                'results': data
-            }
-
-            return self.entity_list(self, data)
+        return self.entity_list(self, data)
