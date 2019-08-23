7b6e5dc1189b48a124b7d533bd502344fffe1d19
Irving Kcam <me@ikcam.com>
2019-08-22 21:43:35 -0500

    Update version

--- bot/maps/run.py
+++ bot/maps/run.py
@@ -5,9 +5,27 @@ from .selenium import MapsSelenium
 from .service import MapService
 
 
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+    else:
+        allFiles.append(fullPath)
+
+    return allFiles
+
+
 def run(*args, **kwargs):
     folder = os.getcwd()
-    for name in os.listdir(folder):
+    for name in getListOfFiles(folder):
         if (
             not os.path.isfile(os.path.join(folder, name)) or
             not name.endswith('.csv')
