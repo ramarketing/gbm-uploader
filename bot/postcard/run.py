e13af3a0c16572431bb71efb3e983b7e607f5f5b
Irving Kcam <me@ikcam.com>
2019-10-25 12:37:02 -0500

    Run forever

--- bot/postcard/run.py
+++ bot/postcard/run.py
@@ -8,23 +8,27 @@ def run(*args, **kwargs):
     object_list = None
     next_ = True
 
-    while next_:
-        if object_list is None:
-            object_list = postcard_service.get_list()
-        else:
-            object_list.get_next_page()
+    while True:
+        while next_:
+            if object_list is None:
+                object_list = postcard_service.get_list()
+            else:
+                object_list.get_next_page()
 
-        next_ = True if object_list.next else False
+            next_ = True if object_list.next else False
 
-        for obj in object_list:
-            if not obj.verification_address and not obj.name:
-                continue
-            obj.patch(status='creating')
-            try:
-                PostcardSelenium(postcard=obj)
-                if obj.recipient:
-                    obj.patch(status='requested')
-                else:
-                    obj.patch(status='created')
-            except MaxRetries:
-                obj.patch(status='not-created')
+            for obj in object_list:
+                if not obj.verification_address and not obj.name:
+                    continue
+                obj.patch(status='creating')
+                try:
+                    PostcardSelenium(postcard=obj)
+                    if obj.recipient:
+                        obj.patch(status='requested')
+                    else:
+                        obj.patch(status='created')
+                except MaxRetries:
+                    obj.patch(status='not-created')
+
+        object_list = None
+        next_ = True
