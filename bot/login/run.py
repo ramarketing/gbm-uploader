b55d40b46a84faca22e193d0efa5dfce7d193d2c
Irving Kcam <me@ikcam.com>
2019-12-19 17:43:54 -0500

    Handle JSONDecodeError

--- bot/login/run.py
+++ bot/login/run.py
@@ -1,8 +1,20 @@
+from json.decoder import JSONDecodeError
 from time import sleep
 
 from .selenium import GMBTaskSelenium
 from .service import GMBTaskService
 from . import constants
+
+
+def _run(object_list):
+    for obj in object_list:
+        obj.patch(status=constants.STATUS_RUNNING)
+        try:
+            GMBTaskSelenium(gmbtask=obj)
+        except Exception as err:
+            obj.patch(
+                status=constants.STATUS_FAIL, status_message=str(err)
+            )
 
 
 def run(*args, **kwargs):
@@ -12,21 +24,17 @@ def run(*args, **kwargs):
 
     while True:
         while next_:
-            if object_list is None:
-                object_list = gmbtask_service.get_list()
-            else:
-                object_list.get_next_page()
+            try:
+                if object_list is None:
+                    object_list = gmbtask_service.get_list()
+                else:
+                    object_list.get_next_page()
 
-            next_ = True if object_list.next else False
-
-            for obj in object_list:
-                obj.patch(status=constants.STATUS_RUNNING)
-                try:
-                    GMBTaskSelenium(gmbtask=obj)
-                except Exception as err:
-                    obj.patch(
-                        status=constants.STATUS_FAIL, status_message=str(err)
-                    )
+                next_ = True if object_list.next else False
+                _run(object_list)
+            except JSONDecodeError:
+                print('Connection error, waiting 5 seconds.')
+                sleep(5)
 
         object_list = None
         next_ = True
--- bot/postcard/run.py
+++ bot/postcard/run.py
@@ -1,3 +1,4 @@
+from json.decoder import JSONDecodeError
 from time import sleep
 
 from selenium.common.exceptions import ElementClickInterceptedException
@@ -7,6 +8,23 @@ from .service import PostcardService
 from ..base .exceptions import CredentialInvalid, MaxRetries
 
 
+def _run(object_list):
+    for obj in object_list:
+        if not obj.verification_address and not obj.name:
+            continue
+        obj.patch(status='creating')
+        try:
+            PostcardSelenium(postcard=obj)
+            if obj.recipient:
+                obj.patch(status='requested')
+            else:
+                obj.patch(status='created')
+        except (MaxRetries, ElementClickInterceptedException):
+            obj.patch(status='not-created')
+        except CredentialInvalid:
+            obj.patch(status='denied')
+
+
 def run(*args, **kwargs):
     postcard_service = PostcardService()
     object_list = None
@@ -14,27 +32,17 @@ def run(*args, **kwargs):
 
     while True:
         while next_:
-            if object_list is None:
-                object_list = postcard_service.get_list()
-            else:
-                object_list.get_next_page()
+            try:
+                if object_list is None:
+                    object_list = postcard_service.get_list()
+                else:
+                    object_list.get_next_page()
 
-            next_ = True if object_list.next else False
-
-            for obj in object_list:
-                if not obj.verification_address and not obj.name:
-                    continue
-                obj.patch(status='creating')
-                try:
-                    PostcardSelenium(postcard=obj)
-                    if obj.recipient:
-                        obj.patch(status='requested')
-                    else:
-                        obj.patch(status='created')
-                except (MaxRetries, ElementClickInterceptedException):
-                    obj.patch(status='not-created')
-                except CredentialInvalid:
-                    obj.patch(status='denied')
+                next_ = True if object_list.next else False
+                _run(object_list)
+            except JSONDecodeError:
+                print('Connection error, waiting 5 seconds.')
+                sleep(5)
 
         object_list = None
         next_ = True
